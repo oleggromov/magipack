2fc13c808005ea5a1bbe7d1174f64cd5febf7807
Oleg Gromov <mail@oleggromov.com>
2021-05-11 23:01:35 +0100

    Add multi-bit support and optional types

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,7 @@
 import BitwiseOptions from './index';
 
+const ERROR_REGEX = /^BitwiseOption/;
+
 describe('BitwiseOptions', () => {
   describe('single bit options', () => {
     let bo: BitwiseOptions;
@@ -11,14 +13,26 @@ describe('BitwiseOptions', () => {
         {name: 'third'},
         {name: 'fourth'},
       ]);
-    })
+    });
 
-    it('reads single options', () => {
+    it('reads single bit options', () => {
       bo.read(13);
       expect(bo.get('first')).toBe(true);
       expect(bo.get('second')).toBe(false);
       expect(bo.get('third')).toBe(true);
       expect(bo.get('fourth')).toBe(true);
+    });
+
+    it('throws without input at all', () => {
+      expect(() => bo.get('first')).toThrowError(ERROR_REGEX);
+    });
+
+    it('sets default values on 0 input', () => {
+      bo.read(0);
+      expect(bo.get('first')).toBe(false);
+      expect(bo.get('second')).toBe(false);
+      expect(bo.get('third')).toBe(false);
+      expect(bo.get('fourth')).toBe(false);
     });
 
     it('ignores unknown options in input', () => {
@@ -38,57 +52,135 @@ describe('BitwiseOptions', () => {
 
     it('throws on getting/setting unknown option', () => {
       bo.read(3);
-      expect(() => bo.get('fifth')).toThrow();
-      expect(() => bo.set('fifth', true)).toThrow();
+      expect(() => bo.get('fifth')).toThrowError(ERROR_REGEX);
+      expect(() => bo.set('fifth', true)).toThrowError(ERROR_REGEX);
     });
 
     it('throws on too many options or large input', () => {
       const MAX_VALUE = Math.pow(2, 32) - 1;
+      const OPTIONS_31 = [
+        {name: '1'},
+        {name: '2'},
+        {name: '3'},
+        {name: '4'},
+        {name: '5'},
+        {name: '6'},
+        {name: '7'},
+        {name: '8'},
+        {name: '9'},
+        {name: '10'},
+        {name: '11'},
+        {name: '12'},
+        {name: '13'},
+        {name: '14'},
+        {name: '15'},
+        {name: '16'},
+        {name: '17'},
+        {name: '18'},
+        {name: '19'},
+        {name: '20'},
+        {name: '21'},
+        {name: '22'},
+        {name: '23'},
+        {name: '24'},
+        {name: '25'},
+        {name: '26'},
+        {name: '27'},
+        {name: '28'},
+        {name: '29'},
+        {name: '30'},
+        {name: '31'},
+      ];
 
-      expect(() => new BitwiseOptions(OPTIONS_31)).not.toThrow();
-      expect(() => new BitwiseOptions(OPTIONS_31.concat({name: '32'}))).toThrow();
+      expect(() => new BitwiseOptions(OPTIONS_31)).not.toThrowError(ERROR_REGEX);
+      expect(() => new BitwiseOptions(OPTIONS_31.concat({name: '32'}))).toThrowError(ERROR_REGEX);
 
       const bo = new BitwiseOptions(OPTIONS_31);
 
-      expect(() => bo.read(MAX_VALUE + 1)).toThrow();
+      expect(() => bo.read(MAX_VALUE + 1)).toThrowError(ERROR_REGEX);
 
       bo.read(MAX_VALUE);
       expect(bo.get('31')).toBe(true);
       expect(bo.get('1')).toBe(true);
     });
   });
-});
 
-const OPTIONS_31 = [
-  {name: '1'},
-  {name: '2'},
-  {name: '3'},
-  {name: '4'},
-  {name: '5'},
-  {name: '6'},
-  {name: '7'},
-  {name: '8'},
-  {name: '9'},
-  {name: '10'},
-  {name: '11'},
-  {name: '12'},
-  {name: '13'},
-  {name: '14'},
-  {name: '15'},
-  {name: '16'},
-  {name: '17'},
-  {name: '18'},
-  {name: '19'},
-  {name: '20'},
-  {name: '21'},
-  {name: '22'},
-  {name: '23'},
-  {name: '24'},
-  {name: '25'},
-  {name: '26'},
-  {name: '27'},
-  {name: '28'},
-  {name: '29'},
-  {name: '30'},
-  {name: '31'},
-]
+  describe('multi-bit options', () => {
+    let bo: BitwiseOptions;
+
+    beforeEach(() => {
+      bo = new BitwiseOptions([
+        {name: 'first_2bit', size: 2},
+        {name: 'second_bool'},
+        {name: 'third_5bit', size: 5},
+        {name: 'fourth_4bit', size: 4},
+      ]);
+    });
+
+    it('reads multi-bit options', () => {
+      bo.read(2542); // 1001,11101,1,10 = 9,29,1,2
+      expect(bo.get('first_2bit')).toBe(2);
+      expect(bo.get('second_bool')).toBe(true);
+      expect(bo.get('third_5bit')).toBe(29);
+      expect(bo.get('fourth_4bit')).toBe(9);
+    });
+
+    it('reads single-bit as a number', () => {
+      const bo = new BitwiseOptions([
+        {name: 'first', type: 'bool'},
+        {name: 'second', type: 'int'},
+        {name: 'third', type: 'int'},
+      ]);
+      bo.read(5);
+      expect(bo.get('first')).toBe(true);
+      expect(bo.get('second')).toBe(0);
+      expect(bo.get('third')).toBe(1);
+    });
+
+    it('sets multi-bit options correctly', () => {
+      bo.set('first_2bit', 0);
+      bo.set('second_bool', true);
+      bo.set('third_5bit', 31);
+      expect(bo.toNumber()).toBe(252);
+    });
+
+    it('throws if option value is incorrect', () => {
+      expect(() => bo.set('fourth_4bit', 16)).toThrowError(ERROR_REGEX);
+      expect(() => bo.set('second_bool', 1)).toThrowError(ERROR_REGEX);
+    });
+
+    it('ignores unknown options in input', () => {
+      bo.read(30851); // 111,1000,10000,0,11
+      expect(bo.get('first_2bit')).toBe(3);
+      expect(bo.get('second_bool')).toBe(false);
+      expect(bo.get('third_5bit')).toBe(16);
+      expect(bo.get('fourth_4bit')).toBe(8);
+    });
+
+    it('throws on getting/setting unknown option', () => {
+      bo.read(0);
+      expect(() => bo.set('no one', 0)).toThrowError(ERROR_REGEX);
+      expect(() => bo.set('hello', 0)).toThrowError(ERROR_REGEX);
+    });
+
+    it('throws on too many bits or large input', () => {
+      const MAX_VALUE = Math.pow(2, 32) - 1;
+      const OPTIONS = [
+        {name: '1', size: 25},
+        {name: '2', size: 5},
+      ];
+
+      expect(() => new BitwiseOptions(OPTIONS)).not.toThrowError(ERROR_REGEX);
+      expect(() => new BitwiseOptions(OPTIONS.concat({name: '3', size: 1}))).not.toThrowError(ERROR_REGEX);
+      expect(() => new BitwiseOptions(OPTIONS.concat({name: '3', size: 2}))).toThrowError(ERROR_REGEX);
+
+      const bo = new BitwiseOptions(OPTIONS);
+
+      expect(() => bo.read(MAX_VALUE + 1)).toThrowError(ERROR_REGEX);
+
+      bo.read(MAX_VALUE);
+      expect(bo.get('1')).toBe(33554431);
+      expect(bo.get('2')).toBe(31);
+    });
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,58 @@
 interface BitwiseOption {
   /**
-   * Default size is 1 bit, that is true/false
-   */
-  size?: number;
-  /**
    * Name of the options used in all methods
    */
   name: string;
+  /**
+   * Default size is 1 bit, that is read as true/false
+   */
+  size?: number;
+  /**
+   * Bool is default for single-bit,
+   * Int is default for multi-bit options
+   */
+  type?: BitwiseOptionType;
 }
+
+type BitwiseOptionType = 'bool' | 'int';
+type BitwiseOptionValue = boolean | number;
 
 const MAX_BIT = 31;
 
 // ToDo: support non-boolean values
 
 export default class BitwiseOptions {
-  supported: BitwiseOption[];
+  supported: Required<BitwiseOption>[];
   options: {
-    [name: string]: boolean;
+    [name: string]: {
+      value: BitwiseOptionValue | undefined;
+      options: Required<BitwiseOption>;
+    };
   } = {};
 
   constructor(options: BitwiseOption[]) {
     let usedBits = 0;
 
     this.supported = options.map(inputOpt => {
-      const size = inputOpt.size || 1;
+      const size = this._getDefaultedSize(inputOpt);
 
       usedBits += size;
       if (usedBits > MAX_BIT) {
         throw error(`too many options, only up to ${MAX_BIT + 1} bits supported`);
       }
 
-      return {
-        size,
+      const result: Required<BitwiseOption> = {
         name: inputOpt.name,
+        size,
+        type: this._getOptionType(inputOpt),
       };
+
+      this.options[inputOpt.name] = {
+        value: undefined,
+        options: result,
+      };
+
+      return result;
     });
   }
 
@@ -45,8 +64,16 @@ export default class BitwiseOptions {
     }
 
     for (const option of this.supported) {
-      this.options[option.name] = Boolean(input & Math.pow(2, bit));
-      bit += 1;
+      const type = this._getOptionType(option);
+      const mask = (Math.pow(2, option.size) - 1) << bit;
+      const value = (input & mask) >> bit;
+
+      this.options[option.name] = {
+        value: type === 'bool' ? Boolean(value) : value,
+        options: option,
+      };
+
+      bit += option.size;
     }
   }
 
@@ -55,34 +82,75 @@ export default class BitwiseOptions {
     let bit = 0;
 
     for (const option of this.supported) {
-      const value = this.options[option.name] ? Math.pow(2, bit) : 0;
-      result |= value;
-      bit += 1;
+      result |= Number(this.options[option.name].value) << bit;
+      bit += option.size;
     }
 
     return result;
   }
 
-  get(name: string): boolean {
+  get(name: string): BitwiseOptionValue {
     this._throwOnUnsupportedOption(name);
+    this._throwOnNoValue(name);
 
-    return this.options[name];
+    return this.options[name].value as BitwiseOptionValue;
   }
 
-  set(name: string, value: boolean): void {
+  set(name: string, value: BitwiseOptionValue): void {
     this._throwOnUnsupportedOption(name);
+    this._throwOnTypeMismatch(name, value);
 
-    this.options[name] = value;
+    this.options[name].value = value;
   }
 
   _throwOnUnsupportedOption(name: string) {
     if (!this._isOptionSupported(name)) {
-      throw error(`unsupported option ${name}`);
+      throw error(`unsupported option "${name}"`);
+    }
+  }
+
+  _throwOnTypeMismatch(name: string, value: BitwiseOptionValue) {
+    const type = this.options[name].options.type;
+
+    if ((type === 'bool' && typeof value === 'number')
+      || (type === 'int' && typeof value === 'boolean')) {
+        throw error(`unsupported value of type "${typeof value}" for option "${name}" of type "${type}"`);
+      }
+
+    if (type === 'int') {
+      const bits = this.options[name].options.size;
+      const maxVaue = Math.pow(2, bits) - 1;
+      if (value > maxVaue) {
+        throw error(`number ${value} is too big for a ${bits}-bit integer`);
+      }
+    }
+  }
+
+  _throwOnNoValue(name: string) {
+    if (typeof this.options[name].value === 'undefined') {
+      throw error(`option "${name}" value is unset`);
     }
   }
 
   _isOptionSupported(name: string) {
     return this.supported.findIndex(opt => name === opt.name) !== -1;
+  }
+
+  _getOptionType(option: BitwiseOption): BitwiseOptionType {
+    const size = this._getDefaultedSize(option);
+
+    if (size > 1) {
+      if (option?.type === 'bool') {
+        throw error(`unsupported bool type for option "${option.name} of size ${size}`);
+      }
+      return 'int';
+    }
+
+    return option.type || 'bool';
+  }
+
+  _getDefaultedSize(option: BitwiseOption): number {
+    return option.size || 1;
   }
 }
 
